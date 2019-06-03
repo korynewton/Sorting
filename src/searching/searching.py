fef2eb5d50cb817ecc515354f88924157194e423
Kory Newton <korybnewton@gmail.com>
2019-06-03 15:56:16 -0700

    completes binary_search

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -20,12 +20,22 @@ def binary_search(arr, target):
     low = 0
     high = len(arr)-1
 
-    # TO-DO: add missing code
+    while low <= high:
+        mid = (high+low)//2
+        if target < arr[mid]:
+            high = mid - 1
+        elif target > arr[mid]:
+            low = mid + 1
+        elif target == arr[mid]:
+            return mid
+    return -1
 
-    return -1  # not found
 
+print(binary_search([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 4))
 
 # STRETCH: write a recursive implementation of Binary Search
+
+
 def binary_search_recursive(arr, target, low, high):
 
     middle = (low+high)//2
